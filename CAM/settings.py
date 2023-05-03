c66eea052ee45349dc9e3a1ffe381c7c4cabbe2f
jidrovoc27 <jidrovoc@unemi.edu.ec>
2023-05-03 11:21:03 -0500

    Revert "Cambio de base para pythonanywhere"
    This reverts commit 9d0ebde6f1ded0fe77516fe2bb81f40564dacc77.

--- CAM/settings.py
+++ CAM/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'CAM.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-# 'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'CAM',  # Or path to database file if using sqlite3.
-#         'USER': 'postgres',  # Not used with sqlite3.
-#         'PASSWORD': '1234',  # Not used with sqlite3.
-#         'HOST': '127.0.0.1',  # Set to empty string for localhost. Not used with sqlite3.
-#         'PORT': '5432',  # Set to empty string for default. Not used with sqlite3.
-#     },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'CAM',  # Or path to database file if using sqlite3.
+        'USER': 'postgres',  # Not used with sqlite3.
+        'PASSWORD': '1234',  # Not used with sqlite3.
+        'HOST': '127.0.0.1',  # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',  # Set to empty string for default. Not used with sqlite3.
+    },
 }
 
 
