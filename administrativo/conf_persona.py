ddcb46dc142ba2535244032603930b72b448c608
jidrovoc <john_idrovo_16@outlook.com>
2023-09-26 17:01:35 -0500

    Crear perfiles

--- administrativo/conf_persona.py
+++ administrativo/conf_persona.py
@@ -2,10 +2,11 @@ import sys
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db import transaction
+from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import render
 from administrativo.funciones import add_data_aplication
-from administrativo.models import Persona
+from administrativo.models import Persona, PersonaPerfil, Docente, Alumno
 
 
 @login_required(redirect_field_name='next', login_url='/login')
@@ -18,17 +19,106 @@ def view_persona(request):
         if 'peticion' in request.POST:
             peticion = request.POST['peticion']
 
-            if peticion == 'eliminar_persona':
+            if peticion == 'crear_perfil':
+                try:
+                    tipo = request.POST['tipo']
+                    idpersona = request.POST['id']
+                    filtro = (Q(status=True) & Q(persona_id=idpersona))
+
+                    if tipo == 'Administrativo':
+                        filtro = filtro & Q(is_administrador=True)
+                    elif tipo == 'Docente':
+                        filtro = filtro & Q(is_profesor=True)
+                    elif tipo == 'Alumno':
+                        filtro = filtro & Q(is_alumno=True)
+                    perfil_ = PersonaPerfil.objects.filter(filtro)
+                    if not perfil_.exists():
+                        newperfil = PersonaPerfil(persona_id=idpersona)
+                        newperfil.save(request)
+                        if tipo == 'Administrativo':
+                            newperfil.is_administrador = True
+                            newperfil.save(request)
+                        elif tipo == 'Docente':
+                            newperfil.is_profesor = True
+                            newperfil.save(request)
+                        elif tipo == 'Alumno':
+                            newperfil.is_alumno = True
+                            newperfil.save(request)
+                    else:
+                        return JsonResponse({"respuesta": False, "mensaje": 'La persona ya cuenta con perfil'})
+
+
+
+                except Exception as ex:
+                    return JsonResponse({"respuesta": False, "mensaje": 'Error al crear el perfil a la persona'})
+
+            elif peticion == 'activar_perfil':
+                try:
+                    tipo = request.POST['tipo']
+                    idpersona = request.POST['id']
+                    filtro = (Q(status=False) & Q(persona_id=idpersona))
+                    perfil_ = PersonaPerfil.objects.filter(filtro)
+
+                    if perfil_.exists():
+                        if tipo == 'Administrativo':
+                            perfil_.is_administrador = True
+                            perfil_.save(request)
+                        elif tipo == 'Docente':
+                            perfil_.is_profesor = True
+                            perfil_.save(request)
+                            consultar_docente = Docente.objects.filter(status=True, persona_id=idpersona)
+                            if not consultar_docente.exists():
+                               crear_docente = Docente(persona_id=idpersona)
+                               crear_docente.save(request)
+                        elif tipo == 'Alumno':
+                            perfil_.is_alumno = True
+                            perfil_.save(request)
+                            consultar_alumno = Alumno.objects.filter(status=True, persona_id=idpersona)
+                            if not consultar_alumno.exists():
+                                crear_alumno = Alumno(persona_id=idpersona)
+                                crear_alumno.save(request)
+                    else:
+                        return JsonResponse({"respuesta": False, "mensaje": 'La persona no cuenta con perfil'})
+
+                    return JsonResponse({"respuesta": True, "mensaje": 'Perfil activado correctamente'})
+                except Exception as ex:
+                    return JsonResponse({"respuesta": False, "mensaje": 'Error al crear el perfil a la persona'})
+
+            elif peticion == 'desactivar_perfil':
+                try:
+                    tipo = request.POST['tipo']
+                    idpersona = request.POST['id']
+                    filtro = (Q(status=False) & Q(persona_id=idpersona))
+                    perfil_ = PersonaPerfil.objects.filter(filtro)
+
+                    if perfil_.exists():
+                        if tipo == 'Administrativo':
+                            perfil_.is_administrador = False
+                            perfil_.save(request)
+                        elif tipo == 'Docente':
+                            perfil_.is_profesor = False
+                            perfil_.save(request)
+                        elif tipo == 'Alumno':
+                            perfil_.is_alumno = False
+                            perfil_.save(request)
+                    else:
+                        return JsonResponse({"respuesta": False, "mensaje": 'La persona no cuenta con perfil'})
+
+                    return JsonResponse({"respuesta": True, "mensaje": 'Perfil activado correctamente'})
+                except Exception as ex:
+                    return JsonResponse({"respuesta": False, "mensaje": 'Error al crear el perfil a la persona'})
+
+            elif peticion == 'eliminar_persona':
                 try:
                     if request.user.is_superuser:
                         persona_delete = Persona.objects.get(id=int(request.POST['id']))
                         persona_delete.status = False
                         persona_delete.save(request)
-                        return JsonResponse({"respuesta": True, "mensaje": "Persona eliminado correctamente."})
+                        return JsonResponse({"respuesta": True, "mensaje": "Persona eliminado correctamente"})
                 except Exception as ex:
-                    return JsonResponse({"respuesta": False, "mensaje": 'Error al eliminar a la persona.'})
+                    return JsonResponse({"respuesta": False, "mensaje": 'Error al eliminar a la persona'})
 
-        return JsonResponse({"respuesta": False, "mensaje": "acción Incorrecta."})
+        return JsonResponse({"respuesta": False, "mensaje": "acción Incorrecta"})
     else:
         if 'peticion' in request.GET:
             peticion = request.GET['peticion']
