a11a806344bbaaf0dc5fc16c40fceb3eaad9d22e
jidrovoc <john_idrovo_16@outlook.com>
2024-05-27 12:19:43 -0500

    .

--- academia/views.py
+++ academia/views.py
@@ -1429,8 +1429,7 @@ def dashboard(request):
                         # data['mis_perfiles'] = mis_perfiles
                         data['alumno'] = alumno = Persona.objects.get(id=persona_logeado.id)
                         data['inscrito'] = inscrito = InscritoCursoA.objects.filter(status=True,
-                                                                                    inscrito=alumno).order_by(
-                            'curso_id').distinct('curso_id').values_list('curso_id')
+                                                                                    inscrito=alumno)
                         data['miscursos'] = CursoA.objects.filter(status=True, id__in=inscrito)
                         data['is_cursos'] = True
                     return render(request, "academia/alumno/view.html", data)
