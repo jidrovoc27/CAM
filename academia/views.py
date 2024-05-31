c9d969384071a11104c9b62feb7deeeaa390a68c
jidrovoc <john_idrovo_16@outlook.com>
2024-05-30 21:49:05 -0500

    .

--- academia/views.py
+++ academia/views.py
@@ -368,7 +368,7 @@ def dashboard(request):
                         recurso.save(request)
                         if recurso.tipo == 1:
                             if 'archivo' in request.FILES:
-                                archivo = form.cleaned_data['archivo']
+                                archivo = request.FILES['archivo']
                                 recurso.archivo = archivo
                                 recurso.save()
                                 return JsonResponse({"respuesta": True, "mensaje": "Recurso actualizado correctamente."})
