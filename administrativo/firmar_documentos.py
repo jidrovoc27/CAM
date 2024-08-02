0bc6566fe8211a8befa4d41e1874514222bf1ed7
jidrovoc <john_idrovo_16@outlook.com>
2024-08-01 19:14:45 -0500

    .

--- administrativo/firmar_documentos.py
+++ administrativo/firmar_documentos.py
@@ -1,25 +1,25 @@
 # # *-* coding: utf-8 *-*
-# import datetime
-# import os
-# import sys
+import datetime
+import os
+import sys
 # import fitz
 # import qrcode
-# from cryptography.hazmat import backends
-# from cryptography.hazmat.primitives.serialization import pkcs12
-# from django.contrib import messages
-# from endesive.pdf import cms
-# from PIL import Image
-# import random
+from cryptography.hazmat import backends
+from cryptography.hazmat.primitives.serialization import pkcs12
+from django.contrib import messages
+from endesive.pdf import cms
+from PIL import Image
+import random
 #
-# from endesive.signer import cert2asn
-# from pdf2image import convert_from_bytes
-# from datetime import datetime
-# from CAM.settings import SITE_STORAGE, SITE_POPPLER, MEDIA_URL, MEDIA_ROOT, DEBUG, BASE_DIR
-# from administrativo.funciones import conviert_html_to_pdf
-# import pyqrcode
-# from PIL import Image, ImageDraw, ImageFont
-# from io import BytesIO
-# import base64
+from endesive.signer import cert2asn
+from pdf2image import convert_from_bytes
+from datetime import datetime
+from CAM.settings import SITE_STORAGE, SITE_POPPLER, MEDIA_URL, MEDIA_ROOT, DEBUG, BASE_DIR
+from administrativo.funciones import conviert_html_to_pdf
+import pyqrcode
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+import base64
 #
 # URL_VALIDADOR='https://ws.firmadigital.gob.ec'
 #
@@ -105,82 +105,82 @@
 #         buffered)
 #
 #
-# def generarfirmaimagen(request, persona, nombrefirmante):
-#     try:
-#         data = {}
-#         folder_pdf = os.path.join(os.path.join(SITE_STORAGE, 'media', 'firmaelectronicapdf', ''))
-#         folder_images = os.path.join(os.path.join(SITE_STORAGE, 'media', 'firmaelectronicaimages', ''))
-#         os.makedirs(folder_pdf, exist_ok=True)
-#         os.makedirs(folder_images, exist_ok=True)
-#         qrname = 'qr_firma_{}'.format(persona.id)
-#         rutapdf = '{}{}.pdf'.format(folder_pdf, qrname)
-#         rutaimg = '{}{}.png'.format(folder_images, qrname)
-#         if os.path.isfile(rutapdf):
-#             os.remove(rutaimg)
-#             os.remove(rutapdf)
-#         url = pyqrcode.create(f'FIRMADO POR: {nombrefirmante}\nRAZON:\nLOCALIZACION: \nFECHA: {datetime.datetime.now()}\nVALIDAR CON: www.firmadigital.gob.ec\n2.10.1')
-#         url.png('{}/{}.png'.format(folder_pdf, qrname), 16, '#000000')  # '#1C3247'
-#         url_path = request.build_absolute_uri('/')[:-1].strip("/")
-#         data['url_path'] = url_path
-#         data['qrname'] = '{}'.format(qrname)
-#         data['persona'] = persona
-#         data['nombrefirmante'] = nombrefirmante
-#         valida = conviert_html_to_pdf('adm_firmardocumentos/firma/vistapreviafirma.html', {'data': data})
-#         if valida:
-#             images = convert_from_bytes(valida.content,
-#                                         output_folder=folder_images,
-#                                         poppler_path=SITE_POPPLER,
-#                                         output_file=f'{qrname}_firma',
-#                                         fmt="png")  # dpi=95, size=(150, 45)
-#             return images[0].filename, True, ''
-#         else:
-#             return f'', False, f'Intentelo nuevamente'
-#     except Exception as ex:
-#         linea_ = sys.exc_info()[-1].tb_lineno
-#         print(ex, linea_)
-#         messages.warning(request, str(ex))
-#         return f'/static/firmaelectronica/firma_position.jpg', False, f'{ex} - Linea: {linea_}'
+def generarfirmaimagen(request, persona, nombrefirmante):
+    try:
+        data = {}
+        folder_pdf = os.path.join(os.path.join(SITE_STORAGE, 'media', 'firmaelectronicapdf', ''))
+        folder_images = os.path.join(os.path.join(SITE_STORAGE, 'media', 'firmaelectronicaimages', ''))
+        os.makedirs(folder_pdf, exist_ok=True)
+        os.makedirs(folder_images, exist_ok=True)
+        qrname = 'qr_firma_{}'.format(persona.id)
+        rutapdf = '{}{}.pdf'.format(folder_pdf, qrname)
+        rutaimg = '{}{}.png'.format(folder_images, qrname)
+        if os.path.isfile(rutapdf):
+            os.remove(rutaimg)
+            os.remove(rutapdf)
+        url = pyqrcode.create(f'FIRMADO POR: {nombrefirmante}\nRAZON:\nLOCALIZACION: \nFECHA: {datetime.datetime.now()}\nVALIDAR CON: www.firmadigital.gob.ec\n2.10.1')
+        url.png('{}/{}.png'.format(folder_pdf, qrname), 16, '#000000')  # '#1C3247'
+        url_path = request.build_absolute_uri('/')[:-1].strip("/")
+        data['url_path'] = url_path
+        data['qrname'] = '{}'.format(qrname)
+        data['persona'] = persona
+        data['nombrefirmante'] = nombrefirmante
+        valida = conviert_html_to_pdf('adm_firmardocumentos/firma/vistapreviafirma.html', {'data': data})
+        if valida:
+            images = convert_from_bytes(valida.content,
+                                        output_folder=folder_images,
+                                        poppler_path=SITE_POPPLER,
+                                        output_file=f'{qrname}_firma',
+                                        fmt="png")  # dpi=95, size=(150, 45)
+            return images[0].filename, True, ''
+        else:
+            return f'', False, f'Intentelo nuevamente'
+    except Exception as ex:
+        linea_ = sys.exc_info()[-1].tb_lineno
+        print(ex, linea_)
+        messages.warning(request, str(ex))
+        return f'/static/firmaelectronica/firma_position.jpg', False, f'{ex} - Linea: {linea_}'
 #
 #
-# def firmar(request, password, certificado, pdf, numPagina=1, x=0, y=0, width=0, height=0):
-#     # pass
-#     persona = request.session['persona']
-#     date = datetime.datetime.now()
-#     date = (datetime.datetime.now() + datetime.timedelta(hours=5)).strftime("D:%Y%m%d%H%M%S+00'00'")
-#     p12 = pkcs12.load_key_and_certificates(certificado.read(), password.encode("ascii"), backends.default_backend())
-#     # VALIDACION DE FIRMA
-#     cert = cert2asn(p12[1])
-#     entevalidador = cert.issuer.native
-#     sujeto = cert.subject.native
-#     nombrefirmante_ = cert.subject.native['common_name']
-#     # if not nombrefirmante_ == persona.nombre_completo():
-#     #     return False, f'Propietario de la firma ({nombrefirmante_}) no coincide con el usuario de la sesión actual {persona.nombre_completo()}'
-#     if datetime.datetime.now().date() >= p12[1].not_valid_after.date():
-#         return False, f'Firma caduco el {p12[1].not_valid_after.date()}'
-#     url_img, resp_img, msg_img = generarfirmaimagen(request, persona, nombrefirmante_)
-#     if not resp_img:
-#         return False, f'Fallo al generar firma. {msg_img}'
-#     imgFirma = Image.open(url_img)
-#     dct = {
-#         "aligned": 0,
-#         "sigflags": 1,
-#         "sigflagsft": 132,
-#         "sigpage": numPagina,  # NUM PAGINA
-#         "auto_sigfield": True,
-#         "sigandcertify": True,  # 612 - width, 792 - height
-#         "signaturebox": (x, y, x + width, y + height),  # UBICACION X, Y, TAMAÑO X, Y
-#         # "signature": f'firmado Fecha',
-#         "signature_img_distort": False,
-#         "signature_img": imgFirma,
-#         "contact": persona.emailinst,
-#         "location": f"",
-#         "signingdate": date,
-#         "reason": "",
-#         "password": password,
-#     }
-#     datau = pdf.read() if type(pdf).__name__.lower() != "bytes" else pdf
-#     datas = cms.sign(datau, dct, p12[0], p12[1], p12[2], "sha256")
-#     return datau, datas
+def firmar(request, password, certificado, pdf, numPagina=1, x=0, y=0, width=0, height=0):
+    # pass
+    persona = request.session['persona']
+    date = datetime.datetime.now()
+    date = (datetime.datetime.now() + datetime.timedelta(hours=5)).strftime("D:%Y%m%d%H%M%S+00'00'")
+    p12 = pkcs12.load_key_and_certificates(certificado.read(), password.encode("ascii"), backends.default_backend())
+    # VALIDACION DE FIRMA
+    cert = cert2asn(p12[1])
+    entevalidador = cert.issuer.native
+    sujeto = cert.subject.native
+    nombrefirmante_ = cert.subject.native['common_name']
+    # if not nombrefirmante_ == persona.nombre_completo():
+    #     return False, f'Propietario de la firma ({nombrefirmante_}) no coincide con el usuario de la sesión actual {persona.nombre_completo()}'
+    if datetime.datetime.now().date() >= p12[1].not_valid_after.date():
+        return False, f'Firma caduco el {p12[1].not_valid_after.date()}'
+    url_img, resp_img, msg_img = generarfirmaimagen(request, persona, nombrefirmante_)
+    if not resp_img:
+        return False, f'Fallo al generar firma. {msg_img}'
+    imgFirma = Image.open(url_img)
+    dct = {
+        "aligned": 0,
+        "sigflags": 1,
+        "sigflagsft": 132,
+        "sigpage": numPagina,  # NUM PAGINA
+        "auto_sigfield": True,
+        "sigandcertify": True,  # 612 - width, 792 - height
+        "signaturebox": (x, y, x + width, y + height),  # UBICACION X, Y, TAMAÑO X, Y
+        # "signature": f'firmado Fecha',
+        "signature_img_distort": False,
+        "signature_img": imgFirma,
+        "contact": persona.emailinst,
+        "location": f"",
+        "signingdate": date,
+        "reason": "",
+        "password": password,
+    }
+    datau = pdf.read() if type(pdf).__name__.lower() != "bytes" else pdf
+    datas = cms.sign(datau, dct, p12[0], p12[1], p12[2], "sha256")
+    return datau, datas
 #
 # def obtener_posicion_x_y_saltolinea(urlpdf, palabras, ultima_pagina=True, exacta=False, horizontal=False):
 #     # COORDENADAS QUE SE OBTIENE ES X Y SUPERIOR IZQUIERDO
--- administrativo/view_firmardocumentos.py
+++ administrativo/view_firmardocumentos.py
@@ -1,124 +1,124 @@
-# # -*- coding: UTF-8 -*-
-# import io
-# import json
-# import os
-# import sys
-# from datetime import datetime
-#
-# from django.contrib import messages
-# from django.contrib.auth.decorators import login_required
-# from django.core.files.base import ContentFile
-# from django.http import HttpResponse, JsonResponse
-# from django.shortcuts import render, redirect
-# from django.db import transaction
-#
-#
-# from administrativo.models import Persona
-# from administrativo.firmar_documentos import firmar, generarfirmaimagen, qrImgFirma, verificarFirmasPDF
-# from administrativo.firmar_documentos_ec import JavaFirmaEc
-# from administrativo.funciones import generar_nombre, quitar_caracteres, remover_caracteres_tildes_unicode, \
-# add_data_aplication
-#
-# @login_required(redirect_field_name='next', login_url='/login')
-# @transaction.atomic()
-# def view_firmardocumentos(request):
-#     global folder_pdf, qrname
-#     data = {}
-#     add_data_aplication(request, data)
-#     usuario_logeado = request.user
-#     if Persona.objects.filter(usuario=usuario_logeado, status=True).exists():
-#         persona_logeado = Persona.objects.get(usuario=usuario_logeado, status=True)
-#     else:
-#         persona_logeado = 'CAM'
-#     data['persona_logeado'] = persona_logeado
-#     if request.method == 'POST':
-#         action = request.POST['action']
-#
-#         if action == 'firmardocumento':
-#             try:
-#                 documento_a_firmar = request.FILES["archivo"]
-#                 certificado = request.FILES["firma"]
-#                 contrasenaCertificado = request.POST['palabraclave']
-#                 razon = request.POST['razon'] if 'razon' in request.POST else ''
-#                 jsonFirmas = json.loads(request.POST['txtFirmas'])
-#                 name_documento_a_firmar, extension_documento_a_firmar = os.path.splitext(documento_a_firmar.name)
-#                 extension_certificado = os.path.splitext(certificado.name)[1][1:]
-#                 bytes_certificado = certificado.read()
-#                 if not jsonFirmas:
-#                     messages.error(request, "Error: Debe seleccionar ubicación de la firma")
-#                     return redirect(request.path)
-#                 for membrete in jsonFirmas:
-#                     datau = JavaFirmaEc(
-#                         archivo_a_firmar=documento_a_firmar, archivo_certificado=bytes_certificado, extension_certificado=extension_certificado,
-#                         password_certificado=contrasenaCertificado,
-#                         page=int(membrete["numPage"]), reason=razon, lx=membrete["x"], ly=membrete["y"]
-#                     ).sign_and_get_content_bytes()
-#                     documento_a_firmar = io.BytesIO()
-#                     documento_a_firmar.write(datau)
-#                     documento_a_firmar.seek(0)
-#                 nombrefile_ = remover_caracteres_tildes_unicode(quitar_caracteres(name_documento_a_firmar)).replace('-', '_').replace('.pdf', '')
-#                 _name = generar_nombre(f'{request.user.username}', nombrefile_)
-#                 response = HttpResponse(documento_a_firmar, content_type="application/pdf")
-#                 response['Content-Disposition'] = f'attachment; filename="{nombrefile_}_firmado.pdf"'
-#                 return response
-#             except Exception as ex:
-#                 messages.error(request, f"{ex} - {sys.exc_info()[-1].tb_lineno}")
-#                 return redirect(request.path)
-#
-#         elif action == 'verificarfirmas':
-#             try:
-#                 archivo = request.FILES['archivo_verificar']
-#                 valido, msg, diccionario = verificarFirmasPDF(archivo)
-#                 return JsonResponse({'result': True, 'context': diccionario})
-#             except Exception as ex:
-#                 return JsonResponse({'result': False, "mensaje": 'Error: {}'.format(ex)}, safe=False)
-#
-#         elif action == 'firmardocumentoold':
-#             try:
-#                 pdf = request.FILES["pdf"]
-#                 firma = request.FILES["firma"]
-#                 passfirma = request.POST['palabraclave']
-#                 txtFirmas = json.loads(request.POST['txtFirmas'])
-#                 if not txtFirmas:
-#                     messages.error(request, "Error: Debe seleccionar ubicación de la firma")
-#                     return redirect(request.path)
-#                     # return JsonResponse({'result': True, "mensaje": "Error: Debe seleccionar ubicación de la firma"}, safe=False)
-#                 generar_archivo_firmado = io.BytesIO()
-#                 x = txtFirmas[-1]
-#                 datau, datas = firmar(request, passfirma, firma, pdf, x["numPage"], x["x"], x["y"], x["width"], x["height"])
-#                 if not datau:
-#                     messages.error(request, f"Error: {datas}")
-#                     return redirect(request.path)
-#                     # return JsonResponse({'result': True, "mensaje": datas}, safe=False)
-#                 generar_archivo_firmado.write(datau)
-#                 generar_archivo_firmado.write(datas)
-#                 generar_archivo_firmado.seek(0)
-#                 extension = pdf._name.split('.')
-#                 tam = len(extension)
-#                 exte = extension[tam - 1]
-#                 nombrefile_ = remover_caracteres_tildes_unicode(quitar_caracteres(pdf._name)).replace('-', '_').replace('.pdf', '')
-#                 _name = generar_nombre(f'{request.user.username}', nombrefile_)
-#                 response = HttpResponse(generar_archivo_firmado, content_type="application/pdf")
-#                 response['Content-Disposition'] = f'attachment; filename="{nombrefile_}_firmado.pdf"'
-#                 # messages.success(request, f'Documento firmado con exito')
-#                 return response
-#                 # return JsonResponse({"result": False, "mensaje": "Guardado con exito", "response": response}, safe=False)
-#             except Exception as ex:
-#                 messages.error(request, "Error: {}".format(ex))
-#                 return redirect(request.path)
-#                 # return JsonResponse({'result': True, "mensaje": "Error: {}".format(ex)}, safe=False)
-#
-#         return JsonResponse({"result": "bad", "mensaje": u"Solicitud Incorrecta."})
-#     else:
-#         try:
-#             qr = qrImgFirma(request, persona_logeado, "png", paraMostrar=True)
-#             data["qrBase64"] = qr[0]
-#
-#             if 'action' in request.GET:
-#                 data['action'] = action = request.GET['action']
-#
-#             data['title'] = f'Firmar documentos (Firma EC)'
-#             return render(request, 'administrativo/firmardocumentos/view.html', data)
-#         except Exception as ex:
-#             texto_notify = 'Error al acceder, {} error en la linea: {}'.format(str(ex), sys.exc_info()[-1].tb_lineno)
-#             print("FALLO : " + texto_notify)
+# -*- coding: UTF-8 -*-
+import io
+import json
+import os
+import sys
+from datetime import datetime
+
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.files.base import ContentFile
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from django.db import transaction
+
+
+from administrativo.models import Persona
+from administrativo.firmar_documentos import firmar, generarfirmaimagen, qrImgFirma, verificarFirmasPDF
+from administrativo.firmar_documentos_ec import JavaFirmaEc
+from administrativo.funciones import generar_nombre, quitar_caracteres, remover_caracteres_tildes_unicode, \
+add_data_aplication
+
+@login_required(redirect_field_name='next', login_url='/login')
+@transaction.atomic()
+def view_firmardocumentos(request):
+    global folder_pdf, qrname
+    data = {}
+    add_data_aplication(request, data)
+    usuario_logeado = request.user
+    if Persona.objects.filter(usuario=usuario_logeado, status=True).exists():
+        persona_logeado = Persona.objects.get(usuario=usuario_logeado, status=True)
+    else:
+        persona_logeado = 'CAM'
+    data['persona_logeado'] = persona_logeado
+    if request.method == 'POST':
+        action = request.POST['action']
+
+        if action == 'firmardocumento':
+            try:
+                documento_a_firmar = request.FILES["archivo"]
+                certificado = request.FILES["firma"]
+                contrasenaCertificado = request.POST['palabraclave']
+                razon = request.POST['razon'] if 'razon' in request.POST else ''
+                jsonFirmas = json.loads(request.POST['txtFirmas'])
+                name_documento_a_firmar, extension_documento_a_firmar = os.path.splitext(documento_a_firmar.name)
+                extension_certificado = os.path.splitext(certificado.name)[1][1:]
+                bytes_certificado = certificado.read()
+                if not jsonFirmas:
+                    messages.error(request, "Error: Debe seleccionar ubicación de la firma")
+                    return redirect(request.path)
+                for membrete in jsonFirmas:
+                    datau = JavaFirmaEc(
+                        archivo_a_firmar=documento_a_firmar, archivo_certificado=bytes_certificado, extension_certificado=extension_certificado,
+                        password_certificado=contrasenaCertificado,
+                        page=int(membrete["numPage"]), reason=razon, lx=membrete["x"], ly=membrete["y"]
+                    ).sign_and_get_content_bytes()
+                    documento_a_firmar = io.BytesIO()
+                    documento_a_firmar.write(datau)
+                    documento_a_firmar.seek(0)
+                nombrefile_ = remover_caracteres_tildes_unicode(quitar_caracteres(name_documento_a_firmar)).replace('-', '_').replace('.pdf', '')
+                _name = generar_nombre(f'{request.user.username}', nombrefile_)
+                response = HttpResponse(documento_a_firmar, content_type="application/pdf")
+                response['Content-Disposition'] = f'attachment; filename="{nombrefile_}_firmado.pdf"'
+                return response
+            except Exception as ex:
+                messages.error(request, f"{ex} - {sys.exc_info()[-1].tb_lineno}")
+                return redirect(request.path)
+
+        elif action == 'verificarfirmas':
+            try:
+                archivo = request.FILES['archivo_verificar']
+                valido, msg, diccionario = verificarFirmasPDF(archivo)
+                return JsonResponse({'result': True, 'context': diccionario})
+            except Exception as ex:
+                return JsonResponse({'result': False, "mensaje": 'Error: {}'.format(ex)}, safe=False)
+
+        elif action == 'firmardocumentoold':
+            try:
+                pdf = request.FILES["pdf"]
+                firma = request.FILES["firma"]
+                passfirma = request.POST['palabraclave']
+                txtFirmas = json.loads(request.POST['txtFirmas'])
+                if not txtFirmas:
+                    messages.error(request, "Error: Debe seleccionar ubicación de la firma")
+                    return redirect(request.path)
+                    # return JsonResponse({'result': True, "mensaje": "Error: Debe seleccionar ubicación de la firma"}, safe=False)
+                generar_archivo_firmado = io.BytesIO()
+                x = txtFirmas[-1]
+                datau, datas = firmar(request, passfirma, firma, pdf, x["numPage"], x["x"], x["y"], x["width"], x["height"])
+                if not datau:
+                    messages.error(request, f"Error: {datas}")
+                    return redirect(request.path)
+                    # return JsonResponse({'result': True, "mensaje": datas}, safe=False)
+                generar_archivo_firmado.write(datau)
+                generar_archivo_firmado.write(datas)
+                generar_archivo_firmado.seek(0)
+                extension = pdf._name.split('.')
+                tam = len(extension)
+                exte = extension[tam - 1]
+                nombrefile_ = remover_caracteres_tildes_unicode(quitar_caracteres(pdf._name)).replace('-', '_').replace('.pdf', '')
+                _name = generar_nombre(f'{request.user.username}', nombrefile_)
+                response = HttpResponse(generar_archivo_firmado, content_type="application/pdf")
+                response['Content-Disposition'] = f'attachment; filename="{nombrefile_}_firmado.pdf"'
+                # messages.success(request, f'Documento firmado con exito')
+                return response
+                # return JsonResponse({"result": False, "mensaje": "Guardado con exito", "response": response}, safe=False)
+            except Exception as ex:
+                messages.error(request, "Error: {}".format(ex))
+                return redirect(request.path)
+                # return JsonResponse({'result': True, "mensaje": "Error: {}".format(ex)}, safe=False)
+
+        return JsonResponse({"result": "bad", "mensaje": u"Solicitud Incorrecta."})
+    else:
+        try:
+            qr = qrImgFirma(request, persona_logeado, "png", paraMostrar=True)
+            data["qrBase64"] = qr[0]
+
+            if 'action' in request.GET:
+                data['action'] = action = request.GET['action']
+
+            data['title'] = f'Firmar documentos (Firma EC)'
+            return render(request, 'administrativo/firmardocumentos/view.html', data)
+        except Exception as ex:
+            texto_notify = 'Error al acceder, {} error en la linea: {}'.format(str(ex), sys.exc_info()[-1].tb_lineno)
+            print("FALLO : " + texto_notify)
